a18d51835e1a4018a84f430eea9d49d27448119d
mcarreon <carryon2095@gmail.com>
2018-06-08 01:55:10 -0700

    added more logic, finished counter, replacing of displayed words

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,23 +33,22 @@ var userInstructions = document.getElementById('game-instructions');
 
 //  arrays  //
 var hangmanWords = [
-  'Kanye',
-  'Schwayze',
-  'Father',
-  'Big Pun',
-  'Tupac',
-  'Lil Wayne',
-  'Bloodstone',
-  'Parliament',
-  'Kodak Black',
-  'Joji',
-  'Mac Dre'
+  'KANYE',
+  'SCHWAYZE',
+  'FATHER',
+  'TUPAC',
+  'BLOODSTONE',
+  'PARLIAMENT',
+  'JOJI'
 ];
-var guessedLetters = [];
+var wordLetters = []; //  array of available correct letters, corresponds to picked word
+var guessedLetters = [];  //  container for past user guesses
 
 //  string  //
-var hiddenWord = '';
-var pickedWord = '';
+var hiddenWord = ''; //  handles displaying the word
+var pickedWordStatic = ''; //  static container for the target word
+var pickedWord = ''; //  target word
+var testWord = "Naruto"; //   hokage
 
 //  ints    //
 var gamesPlayed = 0;
@@ -59,61 +58,167 @@ var guessChances = 10;
 //  bool    //
 var gameStarted = false;
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
   var userInput = event.key;
 
+  //  initialze game if new
   if (gameStarted == false) {
-    if (
-      userInput === ' ' ||
-      userInput === 'Spacebar' ||
-      userInput === 'Enter'
-    ) {
-      gameStarted = true;
-      pickedWord = pickWord();
-      setWord(pickedWord);
-      console.log(pickedWord);
-      userGameStarted.textContent = "Game Started";
-      userInstructions.textContent = "Guess the letter by pressing the corresponding key";
-      userWordToGuess.textContent = hiddenWord;
-      userGamesPlayed.textContent = gamesPlayed;
-      userGamesWon.textContent = gamesWon;
+    initialize(userInput);
+  } 
+  //  if out of guesses, show bio, and reinitialize game for next round
+  else if (guessChances === 0) {
+    reset();
+  }
+  //  if game is already initialized
+  else {
+    //  converts all input to uppercase
+    var input = userInput.toUpperCase();
+
+    //  reads for available correct letters and replaces
+    correctInput(input);
+
+    //  if input hasnt been guessed before, push to guessed letters and decrement chances if bad letter
+    if (!isGuessed(input) && isAlpha(input)) {
+      guessedLetters.push(input);
+      if (!isGoodInput(input)) {
+        guessChances--;
+        userGuessChances.textContent = guessChances;
+      }
+    }
+    //  if letter is wrong, decrement chances
+    if (!isGoodInput(input) && !isGuessed(input) && isAlpha(input)) {
+      console.log(2);
+      guessChances--;
       userGuessChances.textContent = guessChances;
     }
-  } else {
-
+    userGuessedLetters.textContent = guessedLetters;
   }
 };
 
+//picks a word from available word bank
 function pickWord() {
   var word = hangmanWords[Math.floor(Math.random() * hangmanWords.length)];
   return word;
 }
 
+//hides to word on display
 function setWord(word) {
   var wordHide = '';
-
   //    hides the word with _ and ' '
   for (var i = 0; i < word.length; i++) {
+    wordLetters.push(word[i]);
     wordHide = wordHide.concat('_');
     wordHide = wordHide.concat(' ');
   }
+
   hiddenWord = wordHide;
-
-  var spaceLocation = 2 * returnSpace(word);
-  
-
-  console.log(hiddenWord);
-  console.log(hiddenWord.length);
-  console.log(spaceLocation);
-
-  hiddenWord[spaceLocation] = 'A';
-  console.log(hiddenWord);
 }
 
-function returnSpace(word) {
-    for (var i = 0; i < word.length; i++) {
-        if (word[i] === ' ') {
-            return i;
-        }
+//initializes game
+function initialize(userInput) {
+  if (
+    userInput === ' ' ||
+    userInput === 'Spacebar' ||
+    userInput === 'Enter'
+  ) {
+    gameStarted = true;
+    pickedWord = pickWord();
+    pickedWordStatic = pickedWord;
+    setWord(pickedWord);
+    console.log(pickedWord);
+    userGameStarted.textContent = "Game Started";
+    userInstructions.textContent = "Guess the letter by pressing the corresponding key";
+    userWordToGuess.textContent = hiddenWord;
+    userGamesPlayed.textContent = gamesPlayed;
+    userGamesWon.textContent = gamesWon;
+    userGuessChances.textContent = guessChances;
+    userGuessedLetters.textContent = guessedLetters;
+  }
+}
+
+//resets needed variables for fresh round
+function reset() {
+  guessChances = 10;
+  hiddenWord = '';
+  pickedWord = '';
+  guessedLetters = [];
+  wordLetters = [];
+  initialize(' ');
+}
+
+//sets character at specific index in string
+function setChar(string, index, char) {
+  if (index > string.length - 1) {
+    return string;
+  }
+  return string.substr(0, index) + char + string.substr(index + 1);
+}
+
+//returns an array of indexs that from pickedWordStatic that correspond to the char arg
+function findChar(char) {
+  var indexes = [];
+
+  for (var i = 0; i < pickedWordStatic.length; i++) {
+    if (pickedWordStatic[i] == char) {
+      indexes.push(i);
     }
+  }
+  return indexes;
 }
+
+function isGuessed(input) {
+  for (var i = 0; i < guessedLetters.length; i++) {
+    if (input === guessedLetters[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+//returns whether char is a letter 
+function isAlpha(char) {
+  return /^[A-Z]$/i.test(char);
+}
+//reads for available correct letters and replaces correct inputs
+function correctInput(input) {
+  for (var i = 0; i < wordLetters.length; i++) {
+    //  if input is not a letter, break for next input
+    if (!isAlpha(input)) {
+      break;
+    }
+    //  if they guess a letter they already guessed, break for next input
+    else if (isGuessed(input)) {
+      break;
+    }
+    //  if the input is correct, replace chars in hiddenWord, and splice out the corresponding chars from wordLetters 
+    else if (input === wordLetters[i]) {
+      var wordIndex = [];
+      wordIndex = findChar(input);
+
+      //  replaces the _ in the game word 
+      for (var n = 0; n < wordIndex.length; n++) {
+        hiddenWord = setChar(hiddenWord, wordIndex[n] * 2, input);
+      }
+      //  takes input char out of available word letters
+      for (var x = 0; x < wordLetters.length; x++) {
+        if (wordLetters[x] == input) {
+          wordLetters.splice(x, 1);
+        }
+      }
+      
+      userWordToGuess.textContent = hiddenWord;
+    }
+  }
+}
+//checks for good input, kinda wet may fix later
+function isGoodInput(input) {
+  for (var i = 0; i < pickedWordStatic.length; i++) {
+    if (input === pickedWordStatic[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+
+
